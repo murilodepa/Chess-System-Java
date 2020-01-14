a7a836010488d9b46fa4f124470b8b3546eef6f0
Murilo Araujo <murilodepa@gmail.com>
2020-01-14 02:07:41 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@
  */
 package application;
 
+import boardGame.Position;
+
 /**
  *
  * @author Murilo Araujo
@@ -17,7 +19,8 @@ public class Program {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
---
+++ src/boardGame/Position.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardGame;
+
+/**
+ *
+ * @author Murilo Araujo
+ */
+public class Position {
+    
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    } 
+}
