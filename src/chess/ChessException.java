1c8bde9efe4a2c8de356cfe4fb0a755eb069552c
Murilo Araujo <murilodepa@gmail.com>
2020-01-14 14:51:22 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+/**
+ *
+ * @author Murilo Araujo
+ */
+public class ChessException extends RuntimeException{
+    
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
